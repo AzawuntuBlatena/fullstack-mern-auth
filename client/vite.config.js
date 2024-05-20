6aa53062be9cb8dac42876400233040431305d19
AzawuntuBlatena <azabla1227@gmail.com>
2024-05-20 10:30:39 +0300

    add routes and create pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '127.0.0.1',
+    port: 5500,
+    hmr: {
+      host: '127.0.0.1',
+      port: 5500
+    }
+  }
+});
