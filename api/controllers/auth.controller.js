104689dcc2654b7e4898c1e332b3eb5d057d911f
AzawuntuBlatena <azabla1227@gmail.com>
2024-06-02 22:37:48 +0300

    signup done!

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -12,6 +12,6 @@ export const signup = async (req, res, next) => {
     await newUser.save();
     res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    next(errorHandler(300, "something is worng"));
+    next(error);
   }
 };
