196e6353d92194cf844e16905234a260104d0dfc
AzawuntuBlatena <azabla1227@gmail.com>
2024-06-02 19:22:55 +0300

    adding error handling  middleware

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '127.0.0.1',
-    port: 5500,
+    host: "127.0.0.1",
+    port: 5000,
     hmr: {
-      host: '127.0.0.1',
-      port: 5500
-    }
-  }
+      host: "127.0.0.1",
+      port: 5000,
+    },
+  },
 });
